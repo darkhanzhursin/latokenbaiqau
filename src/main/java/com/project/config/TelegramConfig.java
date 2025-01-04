a093dd0be91b6b3039a04ddb4d47a38cdcf0151f
Darkhan Zhursin <daratechn@gmail.com>
2025-01-04 16:00:01 +0500

    delete trigger controller

--- src/main/java/com/project/config/TelegramConfig.java
+++ src/main/java/com/project/config/TelegramConfig.java
@@ -1,6 +1,8 @@
 package com.project.config;
 
 import com.project.bot.TelegramBot;
+import com.project.service.EmbeddingComponent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +12,11 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
+@RequiredArgsConstructor
 @Slf4j
 public class TelegramConfig {
+
+    private final EmbeddingComponent embeddingComponent;
 
     @Bean
     public TelegramBot telegramBot(@Value("${bot.name}") String botName,
@@ -20,6 +25,9 @@ public class TelegramConfig {
         try {
             var telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(telegramBot);
+            log.info("Loading single document start");
+            embeddingComponent.loadSingleDocument();
+            log.info("Loading single document end");
         } catch (TelegramApiException e) {
             log.error("Exception during registration telegram api: {}", e.getMessage());
         }
